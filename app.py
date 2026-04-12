3d71100b5bf9624277533789488c38bb5e7e6db9
SAIFUR-RAHMAN123 <mdrahmansaifur1@gmail.com>
2026-04-12 23:14:21 +0530

    Fix for deployment

--- app.py
+++ app.py
@@ -164,5 +164,8 @@ def get_stats():
         "overdue": overdue
     })
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)

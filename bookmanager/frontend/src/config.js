4e8226ae531f5de5e9bf9aae2f064674ecd7f46d
SAIFUR-RAHMAN123 <mdrahmansaifur1@gmail.com>
2026-04-13 20:10:46 +0530

    final api fix

--- bookmanager/frontend/src/config.js
+++ bookmanager/frontend/src/config.js
@@ -1,2 +1,2 @@
-const API_URL = "https://librarymanagementsystem-production-41e2.up.railway.app";
+const API_URL = "https://librarymanagementsystem-production-0b93.up.railway.app";
 export default API_URL;
